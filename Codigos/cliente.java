ac027073427fab5b9721191fd15be55de2c8a71f
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2021-11-29 16:12:46 -0300

    cadastro do cliente

---
+++ Codigos/cliente.java
@@ -0,0 +1,32 @@
+package CadastroCliente;
+
+public class cliente {
+	// CRIANDO OS PRINCIPAIS ATRIBUTOS DO CLIENTE//
+	
+	private String nome;
+	private String CPF;
+	private String endereco;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
