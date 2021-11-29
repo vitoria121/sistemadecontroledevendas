bbaf2ee352e392216491a28e46ac7a2331757012
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-11-29 17:29:26 -0300

    Add files via upload

---
+++ Codigos/fornecedor.java
@@ -0,0 +1,27 @@
+public class fornecedor {
+
+	
+	    private static String nome;
+	    private static int telefone;
+	    private static int cpf;
+		public static String getNome() {
+			return nome;
+		}
+		public static void setNome(String nome) {
+			fornecedor.nome = nome;
+		}
+		public static int getTelefone() {
+			return telefone;
+		}
+		public static void setTelefone(int telefone) {
+			fornecedor.telefone = telefone;
+		}
+		public static int getCpf() {
+			return cpf;
+		}
+		public static void setCpf(int cpf) {
+			fornecedor.cpf = cpf;
+		}
+		
+	
+}
