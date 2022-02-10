d89b27a3894b14549982bef97a417776b1216c7b
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2022-02-10 13:33:28 -0300

    código atualizado  ( 10/ 02)

--- Codigos/cliente.java
+++ Codigos/cliente.java
@@ -1,19 +1,27 @@
 package CadastroCliente;
 
+import java.util.Scanner;
+import java.util.Set;
+
+// Codigos  atualizados 10/02 //
+
 public class cliente {
 	// CRIANDO OS PRINCIPAIS ATRIBUTOS DO CLIENTE//
 	
-	private String nome;
-	private String CPF;
-	private String endereco;
+	
+	private static String nome;
+	private static String CPF;
+	private static String endereco;
 	
 	
 	
-	public String getNome() {
+	public static String getNome() {
 		return nome;
+	
+		
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setNome(String string) {
+		this.nome = string;
 	}
 	public String getCPF() {
 		return CPF;
@@ -28,13 +36,45 @@ public class cliente {
 		this.endereco = endereco;
 	}
 	
-	// AlteraÃ§Ã£o dos dados do cliente //
-    
-    public void AlteraÃ§Ã£o (String nome, String CPF, String endereco)  {
-        this.nome = nome;
-        this.CPF = CPF;
-        this.endereco = endereco;
-        
-    }
-    
+	public static void Alteração () {
+		
+		Scanner entrada = new Scanner (System.in);
+		System.out.println ("Digite o nome do cliente que deseja fazer a modificação:");
+		String nome1 = entrada.next();
+	    nome1.equals(cliente.getNome());
+		System.out.println ("O que deseja modificar?");
+		System.out.println ("[1] Nome");
+		System.out.println ("[2] endereço");
+		System.out.println ("[3] CPF"); 
+		System.out.println ("[4] sair do sistema");
+		int numero = entrada.nextInt();
+		
+		    if (numero == 1) {
+		    	System.out.println (" digite o nome modificado");
+		    	nome = entrada.next();
+		    	System.out.println (" Nome modificado com sucesso!");
+		    }
+		    	
+		
+		    if  (numero == 2) {
+		    	System.out.println (" digite o endereço modificado");
+		    	endereco = entrada.next();
+		    	System.out.println (" Endereço modificado com sucesso!");
+			
+		} 
+		    if  (numero == 3) {
+		    	System.out.println (" digite o CPF modificado");
+		    	CPF = entrada.next();
+		    	System.out.println (" CPF modificado com sucesso!");
+		    }
+		    
+		    else if (numero == 4) {
+		    	System.out.println ("Saindo do sistema.... bye!");
+		    }
+		
+	}
 }
+
+
+	
+
