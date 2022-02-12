67486e0791ff818eb96972ea1907544d5fe8034a
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2022-02-11 22:51:23 -0300

    vendas

---
+++ Codigos/vendas.java
@@ -0,0 +1,48 @@
+package vendas;
+
+public class vendas {
+	
+	// ATRIBUTOS:
+	private String datadavenda;
+	private String nomedocomprador;
+	private String mercadoriavendida;
+	private double valorvenda;
+	
+	
+	//MÉTODOS:
+	public String getDatadavenda() {
+		return datadavenda;
+	}
+	
+	public void setDatadavenda(String datadavenda) {
+		this.datadavenda = datadavenda;
+	}
+	
+	public String getNomedocomprador() {
+		return nomedocomprador;
+	}
+	
+	public void setNomedocomprador(String nomedocomprador) {
+		this.nomedocomprador = nomedocomprador;
+	}
+	
+	
+	public String getMercadoriavendida() {
+		return mercadoriavendida;
+	}
+	
+	public void setMercadoriavendida(String mercadoriavendida) {
+		this.mercadoriavendida = mercadoriavendida;
+	}
+	
+	public double getValorvenda() {
+		return valorvenda;
+	}
+	
+	public void setValorvenda(double valorvenda) {
+		this.valorvenda = valorvenda;
+	}
+	
+	
+
+}
